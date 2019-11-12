679f1997579cba4082f80711bea334e1293ce9e4
Plokoon1987 <plokoon1987@gmail.com>
2019-11-12 15:41:23 +0000

    New setup for environment variables usage

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ Project created to automate sending emails
 
 Good for sending updated Ip address or other information
 
+python3 sources need to be unlocked before using the script
 variables 'from_email', 'from_email_pass' and 'to_email' need to be set up initially
--- mail_me.py
+++ mail_me.py
@@ -1,9 +1,11 @@
-#! /usr/local/bin/python3
+# Other python3 sources: #! /usr/local/bin/python3
+# Other python3 sources: #! /usr/bin/python3
 '''
     Script Design to work with Gmail mail server
 '''
 
 import sys
+import os
 import subprocess
 import smtplib
 from email.message import EmailMessage
@@ -21,6 +23,13 @@ mail_vars = {
     'from_email_pass': 'password',
     'to_email': ['user1@to_example.ie', 'user2@to_example.com'],
 }
+
+try:
+    mail_vars['from_email'] = os.environ['MAIL_ME_FROM_EMAIL']
+    mail_vars['from_email_pass'] = os.environ['MAIL_ME_FROM_EMAIL_PASS']
+    mail_vars['to_email'] = [os.environ['MAIL_ME_TO_EMAIL_1']]
+except:
+    pass
 
 ##### Getting command kwargs #####
 for elem in sys.argv[1:]:
